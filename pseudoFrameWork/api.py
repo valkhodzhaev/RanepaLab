e1059330ef6b5ab9454c29060e8de22d84f50d64
valkhodzhaev <khodzhaev-v@mail.ru>
2025-09-29 01:39:38 +0300

    futures MarketData + liquidity

--- pseudoFrameWork/api.py
+++ pseudoFrameWork/api.py
@@ -4,40 +4,14 @@ import asyncio
 import aiohttp
 
 
-class MOEXapi(object):
-    """MOEXapi."""
+MOEX_API_URL = r'https://iss.moex.com'
+
+
+async def getAnyMOEX(api_method: str, arguments: dict, field: str, all: bool = False) -> pd.DataFrame:
+    """getAnyMOEX"""
+    async with aiohttp.ClientSession() as session:
+        iss = aiomoex.ISSClient(session, MOEX_API_URL + api_method, arguments)
+
+        data = await iss.get_all() if all else await iss.get()
     
-    def __init__(self):
-        self.api_url = r'https://iss.moex.com/'
-
-    
-    async def get(self, api_method, params, field, all=False):
-        async with aiohttp.ClientSession() as session:
-            iss = aiomoex.ISSClient(
-                session,
-                self.api_url + api_method,
-                params
-            )
-
-            if all:
-                data = await iss.get_all()
-            
-            else:
-                data = await iss.get()
-            
-            data = pd.DataFrame(data[field])
-            return data
-
-                
-
-if __name__ == '__main__':
-    api = MOEXapi()
-    url = r'/iss/statistics/engines/futures/markets/forts/series.json'
-    params = {}
-    field = 'series'
-
-    df = asyncio.run(
-        api.get(url, params, field)
-    )
-
-    pass
+        return pd.DataFrame(data[field])
