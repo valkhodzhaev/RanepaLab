0867e390a617981ecfd02805e8afd0ce2d25c9db
valkhodzhaev <khodzhaev-v@mail.ru>
2025-10-21 09:03:10 +0300

    Question 3 calc is done

--- pseudoFrameWork/api.py
+++ pseudoFrameWork/api.py
@@ -9,7 +9,7 @@ MOEX_API_URL = r'https://iss.moex.com'
 
 async def getAnyMOEX(api_method: str, arguments: dict, field: str, all: bool = False) -> pd.DataFrame:
     """getAnyMOEX"""
-    async with aiohttp.ClientSession() as session:
+    async with aiohttp.ClientSession(trust_env=True) as session:
         iss = aiomoex.ISSClient(session, MOEX_API_URL + api_method, arguments)
 
         data = await iss.get_all() if all else await iss.get()
